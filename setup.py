d42b1079351afdedf96d5372dc9fe409a1edc17f
Eduardo Izquierdo <sirmartin@gmail.com>
2022-04-01 10:57:03 +0200

    Trying things

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='calculator',
     version='0.0.1',
     description='Tdd Calculator',
     author='saamirhye',
-    include_package_data=True
+    include_package_data=True,
+    packages=find_packages(include=['operations'])
 )
